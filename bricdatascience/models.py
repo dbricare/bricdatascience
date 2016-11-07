2c8f615e77d03594641331bcbf3d7d2df176480a
db <db>
2016-11-07 10:52:31 -0800

    update equity landing page text

--- bricdatascience/models.py
+++ bricdatascience/models.py
@@ -40,7 +40,7 @@ def genidx():
              },
              {'name':'candlestick',
              'title':'Equity Price Movement', 'img':'img/stocks.jpg',
-             'text':'Provides elementary information concerning equity prices, movement, dividends, splits, and calculates moving averages.',
+             'text':'Provides elementary information including prices, movement, dividends, splits, and calculates moving averages for 3000 US equities.',
              'doc': 'Web application'
              },
              {'name':'expedia',
